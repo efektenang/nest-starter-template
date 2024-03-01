10da342c1acc91f993c65c61d007f54a53199402
Muhammad Arizky <arizkymuhammad46@gmail.com>
2024-03-01 11:05:02 +0700

    clean code

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Enjoy your coffee😎."', () => {
+      expect(appController.getHello()).toBe('Enjoy your coffee😎.');
     });
   });
 });
