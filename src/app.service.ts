907cfe40922a62c43aee6564a03448bf027720e8
Muhammad Arizky <arizkymuhammad46@gmail.com>
2024-04-30 20:44:19 +0700

    reset

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Enjoy your coffee😎. hello';
+    return 'Enjoy your coffee😎.';
   }
 }
